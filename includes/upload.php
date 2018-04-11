d5d5df130091d602bd2e0bab8c40ff476809ea7c
michebar <38079098+michebar@users.noreply.github.com>
2018-04-11 13:08:48 +0300

    11/4/18

--- includes/upload.php
+++ includes/upload.php
@@ -1,4 +1,23 @@
-<?php
+<?php header('Content-Type: text/html; charset=utf-8'); 
+
+$db = mysql_connect('localhost','1','2');
+mysql_select_db('mydb',$db);
+mysql_query("SET NAMES 'utf8'",$db);
+
+	$servername = "ronipe.mtacloud.co.il";
+	$username = "ronipe";
+	$password = "XgU+2dSlXNJW";
+	$dbname = "ronipe_ishuffle";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+
+
 $target_dir = "/home/ronipe/public_html/PDFconvert/";
 $target_file = $target_dir . basename($_FILES["file"]["name"]);
 $file_type=$_FILES['file']['type'];
