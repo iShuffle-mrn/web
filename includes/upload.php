b25b4fe8425d8ae3233c6cdefbdf784328b57c94
michebar <38079098+michebar@users.noreply.github.com>
2018-04-13 09:21:54 +0300

    13/4/18

--- includes/upload.php
+++ includes/upload.php
@@ -1,45 +1,62 @@
 <?php header('Content-Type: text/html; charset=utf-8'); 
 
-$db = mysql_connect('localhost','1','2');
-mysql_select_db('mydb',$db);
-mysql_query("SET NAMES 'utf8'",$db);
+	$db = mysql_connect('localhost','1','2');
+	mysql_select_db('mydb',$db);
+	mysql_query("SET NAMES 'utf8'",$db);
 
-$servername = "ronipe.mtacloud.co.il";
-$username = "ronipe";
-$password = "XgU+2dSlXNJW";
-$dbname = "ronipe_ishuffle";
+	$servername = "ronipe.mtacloud.co.il";
+	$username = "ronipe";
+	$password = "XgU+2dSlXNJW";
+	$dbname = "ronipe_ishuffle";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+	$course = $_POST['course'];
+	$year = $_POST['year'];
+	$moed = $_POST['moed'];
+	$semester = $_POST['semester'];
+	$numOfQuestions = $_POST['numOfQuestions'];
+	$numOfAnswers = $_POST['numOfAnswers'];
 
-// Check connection
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-} 
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
 
-$target_dir = "/home/ronipe/public_html/PDFconvert/";
-$target_file = $target_dir . basename($_FILES["file"]["name"]);
-$file_type=$_FILES['file']['type'];
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
 
-if ($file_type=="application/pdf") {
-    
-    $ext=explode('.',$_FILES['file']['name']);
-    $extension = $ext[1];
-    $newname='Input';
-    $target_file=$target_dir.$newname.'.'.$extension;
-    
-    if(move_uploaded_file($_FILES["file"]["tmp_name"], $target_file))
-    {
-    echo "The file is uploaded";
+	$target_dir = "/home/ronipe/public_html/PDFconvert/";
+	$target_file = $target_dir . basename($_FILES["file"]["name"]);
+	$file_type=$_FILES['file']['type'];
 
-    }
-    else {
-    echo "Problem uploading file";
-    }
-}
-else {
- echo "You may only upload PDFs.<br>";
-}
+	if ($file_type=="application/pdf") {
+		
+		$ext=explode('.',$_FILES['file']['name']);
+		$extension = $ext[1];
+		$newname='Input';
+		$target_file=$target_dir.$newname.'.'.$extension;
+		
+		if(move_uploaded_file($_FILES["file"]["tmp_name"], $target_file))
+		{
+		echo "The file is uploaded";
 
+		}
+		else {
+		echo "Problem uploading file";
+		}
+	}
 
+	else {
+	 echo "You may only upload PDFs.<br>";
+	}
+
+	$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+	VALUES ('John', 'Doe', 'john@example.com')";
+
+	if ($conn->query($sql) === TRUE) {
+		echo "New record created successfully";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+
+	$conn->close();
 ?>
